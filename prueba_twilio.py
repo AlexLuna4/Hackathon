e670335e16ef06772de7974c190522a8601a2959
B4stech <134745083+B4stech@users.noreply.github.com>
2023-05-28 03:47:24 -0600

    Versión actualizada con el bug del permiso denegado

--- prueba_twilio.py
+++ prueba_twilio.py
@@ -5,15 +5,14 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = os.environ['ACbaedc2db785c81503d2e0841e9101bf6']
-auth_token = os.environ['f26edffa50209bb82baf63f71209e0ff']
+account_sid = "ACbaedc2db785c81503d2e0841e9101bf6"
+auth_token = "f26edffa50209bb82baf63f71209e0ff"
 client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-                              body='Hello there!',
-                              from_='+13157079249',
-                              media_url=['https://demo.twilio.com/owl.png'],
-                              to='+528125786597'
+message = client.messages.create(                          
+                             to="+528125786597",
+                             from_="+13157079249",
+                             body="Hello from Python!"
                           )
 
 print(message.sid)
